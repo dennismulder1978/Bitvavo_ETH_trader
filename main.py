73540e46a2d5d6787382aacd192b6ac7a167d3ff
dennismulder1978 <dnns.mldr@gmail.com>
2023-01-15 10:41:42 +0100

    adjustments

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ balance_EURO = get_balance('EUR')
 price_coin = get_price(coin)
 threshold = 4
 ma_a, ma_b = moving_averages(symbol=coin, a=2, b=5, time_type='5m')
-
+delta_ma = ma_b - ma_a
 
 print(f'Balance COIN {coin}: {balance_coin}')
 print(f'Balance EURO: {balance_EURO}')
 print(f'MA_a: {ma_a}')
 print(f'MA_b: {ma_b}')
-print(f'delta MA: {round((ma_b - ma_a), 1)}')
+print(f'delta MA: {round(delta_ma, 1)}')
 print(f'Price COIN: {price_coin}')
 
 
 # LOG items: Action, Pair, Amount, Error, datetime
-if ((ma_b - ma_a) > 4) & (balance_EURO > 10):
+if (delta_ma > 4) & (balance_EURO > 10):
     print(trade_market_order(coin, 'buy', round(0.99 * (balance_EURO / price_coin), 2)))
     print('BUY')
 
-elif ((ma_b - ma_a) < -4) & (balance_coin > 1):
+elif (delta_ma < -4) & (balance_coin > 0.001):
     print(trade_market_order(coin, 'sell', balance_coin))
     print('SELL')
 else:
